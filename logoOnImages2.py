aa8353b9e54840545959cf23da897dd6f8d36eb4
Mohamed Almefrej <113178714+MohamedMrj@users.noreply.github.com>
2023-07-20 00:58:11 +0200

    Update logoOnImages2.py
    Now the code should properly handle cases where the logo doesn't have an alpha channel, and the IndexError: tuple index out of range error should be resolved.

--- logoOnImages2.py
+++ logoOnImages2.py
@@ -76,10 +76,10 @@ def add_logo_to_media(media_folder, logo_path, output_dir):
             # Resize the logo if needed (height=100, width will be adjusted to maintain aspect ratio)
             logo_clip = logo_clip.resize(height=100)
 
-            # Create a mask image for the logo
+            # Create a mask image for the logo (if the logo has an alpha channel)
             logo_mask = Image.new("L", logo.size, 255)  # Create a new grayscale image with all pixels set to white (255)
-            logo_mask = Image.alpha_composite(logo_mask, logo.split()[3])  # Use the alpha channel of the logo as the mask
-            logo_mask = ImageOps.invert(logo_mask)  # Invert the mask to make transparent parts black
+            if logo.mode in ('RGBA', 'LA') and logo.split()[-1] is not None:
+                logo_mask = ImageOps.invert(logo.split()[-1])  # Invert the alpha channel to make transparent parts black
 
             # Set the position for the logo (e.g., top left corner)
             position = (0, 0)
@@ -93,8 +93,6 @@ def add_logo_to_media(media_folder, logo_path, output_dir):
 
             # Save the video with the logo using libx264 codec for video and aac codec for audio
             composite_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
-
-
 # Example usage
 media_folder = 'path/to/your/media/folder'  # Folder containing your images/videos
 logo_path = 'path/to/your/logo.png'  # Path to the logo image
